e24844fdb559ca7dd983d46123633a644b267e2b
robit <36677806+robit-man@users.noreply.github.com>
2024-09-05 20:50:04 -0700

    Update run_nanollm_container.sh

--- bash_scripts/run_nanollm_container.sh
+++ bash_scripts/run_nanollm_container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Function to check if Riva is running
 check_riva() {
     RIVA_PORT=$1
@@ -19,34 +18,26 @@ check_riva 50051  # Change this to the actual port Riva is running on
 ./dummy_script.sh
 
 # Function to start the container and handle failures
-
-#################################################################
-#								  #
-#  Change the folder structure to reflect where you place this  #
-#  script and run it, also the shared folder 'container_shared' #
-#								  #
-#################################################################
-
-
 start_container() {
     while true; do
         jetson-containers run -v /home/roko/container_shared:/container_shared $(autotag nano_llm) /bin/bash -c "
+            cd /container_shared && \
             while true; do
-                python3 container_shared/simple.py 2>&1 | tee python_log.txt | while read -r line; do
+                python3 llm_settings_demo.py --input-port=6200 2>&1 | tee python_log.txt | while read -r line; do
                     echo \"\$line\"
                     if echo \"\$line\" | grep -q 'InternalError: Check failed: (offset + needed_size <= this->buffer.size)'; then
                         echo 'Detected memory allocation failure. Restarting Python script...'
-                        pkill -f 'python3 container_shared/simple.py'
+                        pkill -f 'python3 llm_settings_demo.py --input-port=6200'
                         sleep 5
                         break
                     elif echo \"\$line\" | grep -q 'Error processing chunk'; then
                         echo 'Detected TTS chunk processing error. Restarting Python script...'
-                        pkill -f 'python3 container_shared/simple.py'
+                        pkill -f 'python3 llm_settings_demo.py --input-port=6200'
                         sleep 5
                         break
                     elif echo \"\$line\" | grep -q 'Exception: Watchdog Timer Expired'; then
                         echo 'Detected Watchdog Timer Expired error. Restarting Python script...'
-                        pkill -f 'python3 container_shared/simple.py'
+                        pkill -f 'python3 llm_settings_demo.py --input-port=6200'
                         sleep 5
                         break
                     fi
